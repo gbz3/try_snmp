9c37ca2d9c119df49e2731759f0ac958c70ad1f9
gbz3 <gebizo3@gmail.com>
2018-09-09 09:10:30 +0900

    init

---
+++ try-snmp/src/test/java/com/github/gbz3/try_snmp/AppTest.java
@@ -0,0 +1,14 @@
+package com.github.gbz3.try_snmp;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.jupiter.api.Test;
+
+public class AppTest {
+
+	@Test
+    public void testApp()
+    {
+        assertThat( true );
+    }
+}
